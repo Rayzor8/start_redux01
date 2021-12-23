0a43760bbc315cbb8d3d5895a1308216c40f4be2
flashback220 <rayhidayat220@gmail.com>
2021-12-23 10:36:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'mont': ['Montserrat', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
